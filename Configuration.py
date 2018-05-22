7026a5cd4b2421f22af81723e513bb34c87ddcd6
pyRob <rob.wagner@outlook.com>
2018-05-21 21:14:18 -0400

    GrainBot v1.0 Release

---
+++ Configuration.py
@@ -0,0 +1,18 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Define variables from config.ini
+BOT_TOKEN = config.get('BOT-INFO', 'BOT_TOKEN')
+BOT_USERNAME = config.get('BOT-INFO', 'BOT_USERNAME')
+BOT_PASSWORD = config.get('BOT-INFO', 'BOT_PASSWORD')
+
+LOGIN_URL = config.get('GAME-INFO', 'LOGIN_URL')
+LAND_URL = config.get('GAME-INFO', 'LAND_URL')
+UPDATE_RATE = config.getint('GAME-INFO', 'UPDATE_RATE')
+ALERT_THRESHOLD = config.getfloat('GAME-INFO', 'ALERT_THRESHOLD')
+LOCALE_DATA = config.get('GAME-INFO', 'LOCALE_DATA')
+EVERYONE_ALERT_THRESHOLD = config.getfloat('GAME-INFO', 'EVERYONE_ALERT_THRESHOLD')
+
+CHANNEL_ID = config.get('DISCORD-INFO', 'CHANNEL_ID')
---
+++ GrainBot.py
@@ -0,0 +1,78 @@
+from gevent import monkey; monkey.patch_socket()
+import grequests
+import asyncio
+from robobrowser import RoboBrowser
+import discord
+import Configuration
+
+# Create Discord client session
+client = discord.Client()
+
+# Build a session and submit log-in data upon initialization
+headers = {
+    "Connection": "keep-alive"
+}
+
+session = grequests.Session()
+session.headers = headers
+br = RoboBrowser(session=session, parser='html.parser', history=True)
+br.open('{url}'.format(url=Configuration.LOGIN_URL))
+
+form = br.get_form()
+form['username'] = '{usr}'.format(usr=Configuration.BOT_USERNAME)
+form['password'] = '{pwd}'.format(pwd=Configuration.BOT_PASSWORD)
+try:
+    br.submit_form(form)
+except SyntaxError:
+    print("Form submission failed. Invalid log-in data.")
+
+
+async def refresh():
+    try:
+        Configuration.config.read('config.ini')
+        print('Configuration reloaded.')
+    except FileNotFoundError:
+        print('Cannot read file /config.ini -- make sure it exists.')
+
+
+async def get_grain_price():
+    previous_price = 0
+
+    while not client.is_closed:
+        await asyncio.sleep(10)
+        br.open('{url}'.format(url=Configuration.LAND_URL))
+        result = br.find('h2', {'class': 'text-bold text-light space-1'}).get_text()
+
+        # replace 'result' commas with whitespace and convert to a float
+        r = result.replace(',', '')
+        price = (float(r))
+        channel = client.get_channel(Configuration.CHANNEL_ID)
+
+        if price > Configuration.EVERYONE_ALERT_THRESHOLD:
+            print('@everyone Grain Price: ' + r)
+            client.send_message(channel, '@everyone Grain Price: ' + r)
+        elif price > Configuration.ALERT_THRESHOLD and previous_price != r:
+            print('Grain Price: ' + r)
+            client.send_message(channel, 'Grain Price: ' + r)
+            previous_price = r
+            await asyncio.sleep(10)
+
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+
+@client.event
+async def on_ready():
+    print('Logged in as ' + client.user.name + ' (' + client.user.id + ')')
+    print('------')
+    print('Tracking grain...')
+    client.loop.create_task(get_grain_price())
+
+try:
+    client.run(Configuration.BOT_TOKEN)
+finally:
+    client.loop.close()
