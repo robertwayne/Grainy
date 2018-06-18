e38e37bc4ac69be76b9f731b521683bef334c60e
pyRob <rob.wagner@outlook.com>
2018-06-17 20:37:57 -0400

    Added database support; updated config.ini

---
+++ Database.py
@@ -0,0 +1,9 @@
+import pymysql
+import Configuration as ini
+
+conn = pymysql.connect(host='{}'.format(ini.DB_HOST),
+                       user='{}'.format(ini.DB_USER),
+                       password='{}'.format(ini.DB_PASS),
+                       db='{}'.format(ini.DB_DATA))
+
+db = conn.cursor()
