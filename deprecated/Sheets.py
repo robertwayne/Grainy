2d7b8789f9868d9d81f21ffd5589e101a886a554
pyRob <rob.wagner@outlook.com>
2018-07-05 23:52:22 -0400

    Removed old files. Updated web folder.

--- deprecated/Sheets.py
+++
@@ -1,9 +0,0 @@
-import json
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('secret.json', scope)
-gc = gspread.authorize(credentials)
-sheet = gc.open("SafehouseStash").sheet1
