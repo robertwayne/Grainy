2a8fbc50358eadc3a8efb88c9f4a03cd2f5c3bd5
pyRob <rob.wagner@outlook.com>
2018-07-20 01:43:29 -0400

    Updated event loop.

--- chatbot/omnibot.py
+++ chatbot/omnibot.py
@@ -4,21 +4,21 @@ import discord
 import asyncio
 from chatbot.client import bot
 import config.configuration as ini
-from chatbot.trackers import grain_alerts
 import chatbot.commands
 
 
 @bot.event
 async def on_member_join(member):
     # rewrite this to use config file and role ID's
-    role_new = discord.utils.get(member.server.roles, name='{}'.format(ini.NEWBIE_ROLE_NAME))
+    role_new = discord.utils.get(member.server.roles, name='Outsiders')
     try:
         await bot.add_roles(member, role_new)
     except Exception as e:
-        await bot.send_message(ini.DEV_CHANNEL_ID, 'Failed to add ' + member + ' to Newbies role. See log for details.')
+        await bot.send_message(bot.get_channel(ini.DEV_CHANNEL_ID), 'Failed to add {} to Outsiders role. See log for details.'.format(member.mention))
         print(e)
     finally:
-        await bot.send_message(ini.DEV_CHANNEL_ID, 'Successfully added ' + member + ' to Newbies role.')
+        await bot.send_message(bot.get_channel(ini.DEV_CHANNEL_ID), 'Successfully added {} to Outsiders role.'.format(member.mention))
+        print('{} joined the server as an Outsider.'.format(member))
 
 
 @bot.event
@@ -31,16 +31,16 @@ async def on_ready():
 
 
 def main():
-    event_loop = asyncio.get_event_loop()
-    try:
+    # event_loop = asyncio.get_event_loop()
+    # try:
         # asyncio.ensure_future(grain_alerts())
-        bot.run(ini.BOT_TOKEN)
-        event_loop.run_forever()
-    except Exception as e:
-        print(e)
-    finally:
-        print('Closing event loop...')
-        event_loop.close()
+    bot.run(ini.BOT_TOKEN)
+    #     # event_loop.run_forever()
+    # except Exception as e:
+    #     print(e)
+    # finally:
+    #     print('Closing event loop...')
+    #     # event_loop.close()
 
 
 if __name__ == '__main__':
