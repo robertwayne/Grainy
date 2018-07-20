f0f566f8b6b4fce0bb5d4f897528ab243f95b0b8
pyRob <rob.wagner@outlook.com>
2018-07-20 01:44:44 -0400

    Minor web stuff; still WIP.

--- web/main.py
+++ web/main.py
@@ -7,9 +7,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def test_page():
+def home():
     home = render_template('index.html')
     return home
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    not_found = render_template('404.html'), 404
+    return not_found
 
 
 if __name__ == "__main__":
